7273b75ee29096c6f546849d61396be8a06e80ff
Max Watson <max.w1@hotmail.com>
2021-05-05 22:45:24 -0500

    completed template of content section

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,17 +15,21 @@ function myFunction(x) {
 
 // For Accordian for mobile category links
 var acc = document.getElementsByClassName("mobile-categories");
+let intViewportWidth = window.innerWidth;
+console.log(intViewportWidth);
+// If on tablet or smaller , make the categories be a droppable clickdown 
+if(intViewportWidth <= 768) {
+    acc[0].addEventListener("click", function() {
+        // Toggle between adding and removing "active" class
+        this.classList.toggle("active"); 
 
-acc[0].addEventListener("click", function() {
-    // Toggle between adding and removing "active" class
-    this.classList.toggle("active"); 
-
-    var panel = this.nextElementSibling; 
-    if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-    }
-    else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-});
+        var panel = this.nextElementSibling; 
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        }
+        else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
 
